a60a32eabd150c507454665a0fc3e545143c4733
Yeridson Ruan Nascimento Teixeira <98131797+Kobra-Dev995@users.noreply.github.com>
2024-06-09 15:15:36 -0300

    Update page.jsx

--- app/helloworld/page.jsx
+++ app/helloworld/page.jsx
@@ -3,7 +3,7 @@ export default function HelloWorld() {
     <>
       <h1>Hello World</h1>
       <h2>area de teste</h2>
-      <h3>testando git</h3>
+      <h3>testando git funciona carai de asa</h3>
     </>
   );
 }
